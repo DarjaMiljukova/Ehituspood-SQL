f4245e97ed1c6d92c3b890f27f493deb10fa1431
DarjaMiljukova <120181585+DarjaMiljukova@users.noreply.github.com>
2023-10-18 14:00:14 +0300

    Update xampp_CREATE_TABLE.sql

--- xampp_CREATE_TABLE.sql
+++ xampp_CREATE_TABLE.sql
@@ -1,13 +1,1 @@
-INSERT INTO Klient (Eesnimi, Perenimi, Aadress, MaksuviisId)
-VALUES
-    ('Alexei', 'Ivanov', 'Kliendi aadress 1', 1),
-    ('Natalia', 'Petrova', 'Kliendi aadress 2', 2),
-    ('Vladimir', 'Smirnov', 'Kliendi aadress 3', 3),
-    ('Elena', 'Sidorova', 'kliendi aadress 4', 4),
-    ('Sergei', 'Mihhailov', 'kliendi aadress 5', 5),
-    ('Olga', 'Kuznetsova', 'Kliendi aadress 6', 6),
-    ('Dmitri', 'Nikolajev', 'kliendi aadress 7', 7),
-    ('Maria', 'Popova', 'Kliendi aadress 8', 8),
-    ('Andrey', 'Fedorov', 'Kliendi aadress 9', 9),
-    ('Ljudmila', 'Solovjova', 'Kliendi aadress 10', 10);
 
