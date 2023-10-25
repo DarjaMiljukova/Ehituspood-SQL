b8017ccd799276573bebde55746a9c97806276e9
DarjaMiljukova <120181585+DarjaMiljukova@users.noreply.github.com>
2023-10-25 20:15:26 +0300

    Update xampp_PROCEDURE.sql

--- xampp_PROCEDURE.sql
+++ xampp_PROCEDURE.sql
@@ -1,1 +1,23 @@
+--protseduur LisaLadu
+BEGIN
+    INSERT INTO Ladu (Nimetus, Aadress)
+    VALUES (Nimetus, Aadress);
+END;
 
+--protseduur LisaKategooria
+BEGIN
+    INSERT INTO Kategooria (Nimi)
+    VALUES (Nimi);
+END;
+
+--protseduur LisaTootaja
+BEGIN
+    INSERT INTO Tootaja (Nimi, Aadress, KontaktTelefon)
+    VALUES (Nimi, Aadress, KontaktTelefon);
+END;
+
+--protseduur LisaMaksuviis
+BEGIN
+    INSERT INTO Maksuviis (Nimetus, Kirjeldus)
+    VALUES (Nimetus, Kirjeldus);
+END;
