eb0f88d0cf50346809cf9b409921add8048ab932
DarjaMiljukova <120181585+DarjaMiljukova@users.noreply.github.com>
2023-10-25 13:19:38 +0300

    Update xampp_TRIGGER.sql

--- xampp_TRIGGER.sql
+++ xampp_TRIGGER.sql
@@ -12,10 +12,10 @@ WHERE t.ToodeId = NEW.ToodeId
 --uuendamine toode ja kategooria
 insert into logi(kuupaev, andmed, kasutaja)
 select NOW(),
-concat('Vanad andmed ',OLD.toode, ', ', k1.kategooria, 
-'  Uued andmed ', NEW.toode, ', ', k2.kategooria),
+concat('Vanad andmed ',OLD.Nimi, ', ', k1.Nimi, 
+'  Uued andmed ', NEW.Nimi, ', ', k2.Nimi),
 user
 from toode t
-inner join kategooria k1 on OLD.maakondID=k1.KategooriaId
-inner join kategooria k2 on NEW.maakondID=k2.KategooriaId
+inner join kategooria k1 on OLD.KategooriaId=k1.KategooriaId
+inner join kategooria k2 on NEW.KategooriaId=k2.KategooriaId
 where t.ToodeId=NEW.ToodeId
