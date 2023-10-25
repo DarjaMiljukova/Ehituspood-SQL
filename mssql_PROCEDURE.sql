b376252079cb67fc2fa68f9146d06d303abf375d
MaksimTse <120181800+MaksimTse@users.noreply.github.com>
2023-10-25 13:49:04 +0300

    Update mssql_PROCEDURE.sql

--- mssql_PROCEDURE.sql
+++ mssql_PROCEDURE.sql
@@ -1,1 +1,28 @@
+CREATE PROCEDURE PR_LisaLadu
+    @Nimetus NVARCHAR(50),
+    @Aadress NVARCHAR(50)
+AS
+BEGIN
+    INSERT INTO Ladu (Nimetus, Aadress)
+    VALUES (@Nimetus, @Aadress);
+END;
+
+CREATE PROCEDURE PR_LisaKategooria
+    @Nimi NVARCHAR(50)
+AS
+BEGIN
+    INSERT INTO Kategooria (Nimi)
+    VALUES (@Nimi);
+END;
+
+CREATE PROCEDURE PR_LisaTootaja
+    @Nimi NVARCHAR(50),
+    @Aadress NVARCHAR(50),
+    @KontaktTelefon NVARCHAR(50)
+AS
+BEGIN
+    INSERT INTO Tootaja (Nimi, Aadress, KontaktTelefon)
+    VALUES (@Nimi, @Aadress, @KontaktTelefon);
+
+END;
 
